d168ca88480e5f1c0128132315998d3c1010b69a
Linux User <vm@alpine-test.my.domain>
2020-08-14 19:44:20 +0000

    added a new profile for creating an image with qemu and mpv running on dwm

---
+++ scripts/mkimg.qemu.sh
@@ -0,0 +1,17 @@
+profile_qemu() {
+	title="QEMU"
+	desc="Alpine standard plus QEMU and MPV, under DWM"
+	profile_base
+	profile_abbrev="qemu"
+	image_ext="iso"
+	arch="x86_64"
+	output_format="iso"
+	kernel_addons="xtables-addons"
+	apks="$apks \
+		xorg-server xf86-input-libinput eudev xf86-video-amdgpu xf86-video-intel xf86-video-qxl xf86-video-modesetting xset \
+		mesa-egl setxkbmap dwm ttf-freefont font-noto-cjk \
+		qemu-system-x86_64 qemu-img qemu-ui-gtk qemu-audio-alsa \
+		samba-server samba-client ntfs-3g p7zip rdesktop ffmpeg \
+		mpv alsa-utils alsa-lib alsaconf \
+		hdparm"
+}
