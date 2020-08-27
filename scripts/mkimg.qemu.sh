a4d01f74ab764d32361b1beb3d7c976dce80e34e
Linux User <vm@alpine-test.my.domain>
2020-08-27 16:12:21 +0000

    main/alpine-baselayout/profile     made it show the username instead of the hostname
    scripts/mkimg.qemu.sh
        added a couple of missing packages

--- scripts/mkimg.qemu.sh
+++ scripts/mkimg.qemu.sh
@@ -8,10 +8,10 @@ profile_qemu() {
 	output_format="iso"
 	kernel_addons="xtables-addons"
 	apks="$apks \
-		xorg-server xf86-input-libinput eudev xf86-video-amdgpu xf86-video-intel xf86-video-qxl xf86-video-modesetting xset \
+		xorg-server xf86-input-libinput eudev xf86-video-amdgpu xf86-video-intel xf86-video-qxl xf86-video-modesetting xset xsetroot\
 		mesa-egl setxkbmap dwm ttf-freefont font-noto-cjk \
 		qemu-system-x86_64 qemu-img qemu-ui-gtk qemu-audio-alsa \
-		samba-server samba-client ntfs-3g p7zip rdesktop ffmpeg \
+		samba-server samba-client samba-common-tools ntfs-3g p7zip rdesktop ffmpeg \
 		mpv alsa-utils alsa-lib alsaconf \
 		hdparm"
 }
