b877606880861b25646ec3aadacf2f1b3318fb3d
Linux User <vm@alpine-test.my.domain>
2020-09-10 16:55:31 +0000

    Restored 'linux-firmware-any' to linux-lts APKBUILD and documented (inside APKBUILD) what is the official workaround for building the kernel on a live system, which are - install all dependencies, except linux-firmware-any, manually and then   use 'abuild -rd' - or just install 'linux-firmware-none' and use 'abuild -r'

--- scripts/mkimg.custom_dwm.sh
+++ scripts/mkimg.custom_dwm.sh
@@ -1,12 +1,11 @@
-profile_custom() {
-	title="Custom VM"
+profile_custom_dwm() {
+	title="Custom DWM"
 	desc="Alpine Linux with a minimal kernel and DWM"
 	profile_base
-	profile_abbrev="customvm"
+	profile_abbrev="custom_dwm"
 	image_ext="iso"
 	arch="x86_64"
 	output_format="iso"
-	kernel_addons="xtables-addons"
 	kernel_flavors="custom"
 	apks="$apks \
 		xorg-server xf86-input-libinput eudev xf86-video-amdgpu xf86-video-qxl xf86-video-modesetting xset xsetroot\
